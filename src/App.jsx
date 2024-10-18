dc9d68fa573be726a14938281f481cb80a3a31be
m_thado <tmiseda@gmail.com>
2024-10-18 10:50:41 -0400

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
-import Header from "./components/Header.jsx";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
+
       <ButtonGradient />
     </>
   );

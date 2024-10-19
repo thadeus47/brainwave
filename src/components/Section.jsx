1c50f49a9f00c635d76e9d997833b2f19f29511f
m_thado <tmiseda@gmail.com>
2024-10-19 10:57:45 -0400

    AI generating section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -20,8 +20,9 @@ const Section = ({
       ${className || ""}`}
     >
       {children}
-      <div className="hidden absolute top-0 left-5 w-0 25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7 5 xl:left-10" />
-      <div className="hidden absolute top-0 right-5 w-0 25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7 5 xl:rigth-10" />
+
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
       {crosses && (
         <>

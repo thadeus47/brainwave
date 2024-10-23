2ef2d112ae56834af46e83aff83f830459325432
m_thado <tmiseda@gmail.com>
2024-10-23 19:25:59 -0400

    benefits section

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;

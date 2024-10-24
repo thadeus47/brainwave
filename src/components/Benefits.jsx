87d293dc26137639242051ace0425a8e89187813
m_thado <tmiseda@gmail.com>
2024-10-24 18:57:37 -0400

    benefits changes

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -6,15 +6,20 @@ const Benefits = () => {
   return (
     <Section id="features">
       <div className="container relative z-2">
-        <Heading className="" title="Chat Smarter, Not Harder with Brainwave" />
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
               key={item.id}
             >
-              <div>
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5>{item.title}</h5>
               </div>
             </div>

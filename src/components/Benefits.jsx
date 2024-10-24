4e1c94b3a4365f7e18ec4d38e9c25448b3b577df
m_thado <tmiseda@gmail.com>
2024-10-24 18:35:54 -0400

    heading change

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,3 +1,4 @@
+import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 
@@ -6,6 +7,19 @@ const Benefits = () => {
     <Section id="features">
       <div className="container relative z-2">
         <Heading className="" title="Chat Smarter, Not Harder with Brainwave" />
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+              key={item.id}
+            >
+              <div>
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Section>
   );

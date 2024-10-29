8ed8d06ff7ead295b7d0c5595a57dfb72ff014da
m_thado <tmiseda@gmail.com>
2024-10-29 19:24:42 -0400

    services section changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -45,6 +45,14 @@ const Services = () => {
 
             <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-39rem border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img src={service2} className="h-full object-cover" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Section>

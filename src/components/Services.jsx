1aba6f70ab852ef4d34d99ef66d0418222d95061
m_thado <tmiseda@gmail.com>
2024-10-29 19:19:32 -0400

    services section change

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
 import Heading from "./Heading";
 import Section from "./Section";
 
@@ -41,6 +42,8 @@ const Services = () => {
                 ))}
               </ul>
             </div>
+
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
         </div>
       </div>

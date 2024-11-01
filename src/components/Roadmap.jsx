84f98f230c538212b1cfef2518684c2f9aa6925c
m_thado <tmiseda@gmail.com>
2024-11-01 17:08:54 -0400

    footer  section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -65,12 +65,12 @@ const Roadmap = () => (
           );
         })}
 
-        {/* <Gradient /> */}
+        <Gradient />
       </div>
 
-      {/* <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
+      <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
         <Button href="/roadmap">Our roadmap</Button>
-      </div> */}
+      </div>
     </div>
   </Section>
 );
